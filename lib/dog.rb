259f1d6b7a92be6eb40a21467bdc20d8b5794d30
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-14 23:06:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,8 +65,19 @@ class Dog
     DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
   end
 
-  def self.find_or_create_by(dog)
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ? AND breed = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name, breed)
 
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
   end
 
   def udpate
